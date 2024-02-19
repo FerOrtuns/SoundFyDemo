c9d06ad9e51a803e7ceab5ba0eed00499e0a6e84
FerOrtuns <Fernando.OrtunoLopez@gmail.com>
2024-02-19 17:53:30 +0100

    delete idea

--- src/view/Main.java
+++ src/view/Main.java
@@ -16,8 +16,6 @@ public class Main {
     public static void main(String[] args) {
 
         SoundFy soundFy = new SoundFyImpl();
-        
-        //hola
 
         TracksFactory tracksF = new TracksFactory();
 
